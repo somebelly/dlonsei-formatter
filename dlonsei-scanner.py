b363d94919a773087e619f752f3dac867d028ae4
somebelly <afsomebelly@gmail.com>
2021-06-29 01:33:21 +0200

    0.6.1 fix scanner

--- dlonsei-scanner.py
+++ dlonsei-scanner.py
@@ -4,7 +4,7 @@ import os
 import sys
 from lib import scanner
 
-if os.path.exists(sys.argv[1]):
+if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
     scanner(sys.argv[1])
 else:
     scanner()
